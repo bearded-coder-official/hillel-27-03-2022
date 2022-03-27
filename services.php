546e3d461211c508bee9125a3f1361d999904804
sp2 <sp2>
2022-03-27 11:32:07 +0300

    saaassa

--- services.php
+++ services.php
@@ -5,9 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contacts!</title>
+    <title>Our Service List!</title>
 </head>
 <body>
+
+<ul>
+    <li>Service 1</li>
+    <li>Service 2</li>
+    <li>Service 3</li>
+</ul>
 
 </body>
 </html>
